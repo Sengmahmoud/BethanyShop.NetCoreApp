5d11e73a86fe79984ed8a2bc9e5628fc3c832bc4
Mahmoud Hassan <progmahmoud099@gmail.com>
2020-05-31 18:18:54 +0200

    create ApplicationUserClass to use it insted of using identityuserclass directly

---
+++ Auth/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace BethanyShop.Auth
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name ="Birth Date")]
+        public DateTime Birthdate { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
--- Migrations/20200531161316_AddApplicationUserClass.Designer.cs
+++ Migrations/20200531161316_AddApplicationUserClass.Designer.cs
@@ -4,14 +4,16 @@ using BethanyShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BethanyShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200531161316_AddApplicationUserClass")]
+    partial class AddApplicationUserClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
